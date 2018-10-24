df68668cb36046b6475283caf31da1a402ccbe87
mehrotrarachit1113 <rachit.mehrotra1113@gmail.com>
2018-10-24 19:58:46 +0530

    Hello World.....................................................................

---
+++ server.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const hbs = require('hbs')
+const fs = require('fs')
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials')
+app.set('view engine' , 'hbs')
+app.use(express.static(__dirname + '/public'));
+
+app.use((req , res , next) => {
+    var timeStamp = new Date().toDateString();
+    var log = `${timeStamp} : ${req.url} ${req.method}`
+    fs.appendFile('server.log' , log + '\n', (err) => {
+        if (err) throw err
+    });
+    next();
+});
+
+app.use((req , res , next)=>{
+    res.render('maintenance.hbs')
+})
+
+hbs.registerHelper('getCurrentYear' ,() =>{
+    return new Date().getFullYear()
+});
+
+hbs.registerHelper('screamIt' , (text) => {
+    return text.toUpperCase()
+})
+
+app.get('/' , (req , res) =>{
+    res.render('home.hbs' ,{
+        pageTitle : 'Home Page',
+        welcomeNote : 'Welcome Dude'
+    })
+});
+
+app.get('/about' , (req , res) =>{
+    res.render('about.hbs',{
+        pageTitle : 'About Page',
+        
+    });
+});
+
+app.get('/bad' , (req , res) => {
+    res.send({
+        error : 'Bad Gateway Request'
+    });
+});
+
+app.listen(3000 , () =>{
+    console.log('The server is up and running')
+});
